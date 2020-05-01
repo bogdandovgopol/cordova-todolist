e987c993db6c6225ec9adaab0eed6274f39bb925
Bogdan Dovgopol <bogdan@dovgopol.dev>
2020-05-01 10:41:59 +1000

    rename buttons and add bootstrap classes

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,8 @@
-var todoListArray = [];
+var todoListStorage = window.localStorage.getItem("todoList") ?? window.localStorage.setItem("todoList", JSON.stringify([{
+    title: "test",
+    done: false
+}]));
+var todoListArray = JSON.parse(todoListStorage);
 
 var app = {
     // Application Constructor
@@ -12,6 +16,7 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function () {
         // this.fetchToDoListData();
+        fetchToDoListData();
         listenToAddButtonClick();
 
     },
@@ -24,25 +29,47 @@ function listenToAddButtonClick() {
     $("#addTodo").click(function () {
         var todoText = $("#todoTxt").val();
         todoListArray.push({title: todoText, done: false});
-        // window.localStorage.setItem("todoList", todoListArray);
+        window.localStorage.setItem("todoList", JSON.stringify(todoListArray));
 
         fetchToDoListData();
     });
 }
 
 function fetchToDoListData() {
-    console.log(JSON.stringify(todoListArray));
     var list = $('#todoList');
 
     list.empty();
 
     todoListArray.forEach(function (item, i) {
-        var li = '<li class="item" data-id="' + i + '"><a class="item-text">' + item.title + '</a><button data-id="' + i + '">Done</button><button onclick="removeItemFromToDoList(' + i + ')">Remove</button></li>';
+
+        var done = "not-done";
+        var doneBtn = '<button class="btn btn-success" onclick="markAsDone(' + i + ')">Done</button>';
+        var removeBtn = '<button class="btn btn-danger" onclick="removeItemFromToDoList(' + i + ')">Remove</button>';
+
+        //check if item is already done
+        if (item.done === true) {
+            done = "done";
+            doneBtn = "";
+        }
+
+        //create list item and append
+        var li = '<li class="item">' +
+            '<a class="item-text ' + done + '">' + item.title + '</a>' +
+            doneBtn +
+            removeBtn +
+            '</li>';
         list.append(li);
     })
 }
 
 function removeItemFromToDoList(index) {
     todoListArray.splice(index, 1);
+    window.localStorage.setItem("todoList", JSON.stringify(todoListArray));
+    fetchToDoListData();
+}
+
+function markAsDone(index) {
+    todoListArray[index].done = true;
+    window.localStorage.setItem("todoList", JSON.stringify(todoListArray));
     fetchToDoListData();
 }
